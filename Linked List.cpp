7989b44c1f2d78baec54610772dd07738a89752a
陳尚霆 <129261367+aakim57787612@users.noreply.github.com>
2023-09-27 12:22:37 +0800

    Add files via upload

---
+++ Linked List.cpp
@@ -0,0 +1,297 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+
+class ListNode {
+	friend class LinkedList;
+public:
+	int data;
+	ListNode* next;
+	ListNode() :data(), next() {};
+	ListNode(int a) :data(a), next() {};
+};
+class LL {
+	friend ostream &operator<<(ostream &, const LL&);
+	friend istream &operator>>(istream&, LL&);
+private:
+	ListNode* first;
+public:
+	LL Crop(int a, int b) {
+		ListNode* current = first, *gg = 0;
+		LL abc;
+		for (int i = 1; i < a; i++) {
+			gg = current->next;
+			current = current->next;
+		}
+		current = current->next;
+		for (int i = 1; i <= b; i++) {
+			abc.Insert(current->data);
+			current = current->next;
+		}
+		gg->next = current;
+		return abc;
+	}
+	void operator+=(int a)
+	{
+		Insert(a);
+	}
+	void Insert(LL a)
+	{
+		ListNode* current = first;
+		if (first == 0) {
+			first = a.first;
+			return;
+		}
+		while (current->next != 0) {
+			current = current->next;
+		}
+		current->next = a.first;
+	}
+	void operator+=(LL a)
+	{
+		ListNode* current = first;
+		while (current->next != 0) {
+			current = current->next;
+		}
+		current->next = a.first;
+	}
+	//testLL2[1] = 1;
+	LL &operator=(int a) {
+		*this = a;
+	}
+	int &operator[](int sub) {
+		ListNode* current = first;
+		for (int i = 1; i <= sub; i++) {
+			current = current->next;
+		}
+		return current->data;
+	}
+	LL() :first() {};
+	LL(int a) :first() {};
+	void ver() {
+		LL newnode;
+		while (first->next != 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+		}
+		newnode.Insert(first->data);
+		first = first->next;
+		*this = newnode;
+	}
+	void Insert(int x) {
+		ListNode *newNode = new ListNode(x);
+		if (first == 0) {                      // 若list沒有node, 令newNode為first
+			first = newNode;
+			return;
+		}
+		ListNode *current = first;
+		while (current->next != 0) {           // Traversal
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+	void Insert(int x, int y) {//x插入的位置 y插入的值
+		LL newnode;
+		while (x >= 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+			x--;
+		}
+		newnode.Insert(y);
+		while (first != 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+		}
+		*this = newnode;
+	}
+	void Insert(int x, int y, int z) {//x插入的位置 y為長度 z插入的值
+		LL newnode;
+		if (first == 0) {
+			for (int i = 1; i <= y; i++)
+				newnode.Insert(z);
+			first = newnode.first;
+			return;
+		}
+		while (x >= 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+			x--;
+		}
+		while (y > 0) {
+			newnode.Insert(z);
+			y--;
+		}
+		while (first != 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+		}
+		*this = newnode;
+	}
+	void Delete(int x, int y) {//x插入的位置 y為長度
+		LL newnode;
+		while (x >= 1) {
+			newnode.Insert(first->data);
+			first = first->next;
+			x--;
+		}
+		while (y > 0) {
+			first = first->next;
+			if (first == 0)
+				break;
+			y--;
+		}
+		while (first != 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+		}
+		*this = newnode;
+	}
+	int Head() {
+		int a = first->data;
+		headPtr = first;
+		return a;
+	}
+	int Tail() {
+		ListNode* current = first;
+		while (current->next != 0) {
+			current = current->next;
+		}
+		int a = current->data;
+		tailPtr = current;
+		return a;
+	}
+	int getLength() {
+		ListNode* current = first;
+		int i = 0;
+		while (current->next != 0) {
+			current = current->next;
+			i++;
+		}
+		i++;
+		return i;
+	}
+	void Sort() {
+		ListNode* current = first;
+		int i = getLength();
+		for (int j = 1; j < i; j++) {
+			for (int k = 1; k < i; k++) {
+				if (current->data < current->next->data) {
+					swap(k);
+					current = current->next;
+				}
+				else current = current->next;
+			}
+			current = first;
+		}
+	}
+	void swap(int k) {
+		ListNode* current = first;
+		LL abc;
+		for (int i = 1; i < k; i++) {
+			abc.Insert(current->data);
+			current = current->next;
+		}
+		int a = current->data, b = current->next->data;
+		abc.Insert(b);
+		abc.Insert(a);
+		current = current->next;
+		while (current->next != 0) {
+			current = current->next;
+			abc.Insert(current->data);
+		}
+		first = abc.first;
+	}
+	ListNode* headPtr;
+	ListNode* tailPtr;
+};
+
+ostream& operator<<(ostream& output, const LL& abc) {
+	if (abc.first == 0) {                      // 如果first node指向NULL, 表示list沒有資料
+		output << "List is empty.\n";
+		return output;
+	}
+	ListNode *current = abc.first;            // 用pointer *current在list中移動
+	while (current != 0) {                 // Traversal
+		output << current->data;
+		if (current->next != 0)
+			output << ", ";
+		current = current->next;
+	}
+	output << endl;
+	return output;
+}
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	LL testLL;
+	testLL.Insert(2); // add new value in the end of list
+	testLL.Insert(3);
+	testLL.Insert(1);
+	testLL.Insert(7);
+	testLL.Insert(9);
+	cout << "LL1 = " << testLL << endl; // LL1 = 2, 3, 1, 7, 9
+	cout << "Head:" << testLL.Head() << endl;
+	cout << "Tail:" << testLL.Tail() << endl << endl;
+	cout << "Length:" << testLL.getLength() << endl << endl;
+
+	cout << "insert(2, 5)" << endl;
+	testLL.Insert(2, 5); // (pos, value)
+	cout << testLL << endl << endl;
+
+	cout << "insert(3, 2, -1)" << endl;
+	testLL.Insert(3, 2, -1); // (pos, lenght, value)
+	cout << testLL << endl << endl;
+
+	cout << "delete(1, 2)" << endl;
+	testLL.Delete(1, 2); // (pos, lenght)
+	cout << testLL << endl << endl;
+
+	cout << "delete(2, 2)" << endl;
+	testLL.Delete(2, 2); // (pos, lenght)
+	cout << testLL << endl << endl;
+
+	cout << "sort" << endl;//大到小
+	testLL.Sort(); // apply bubble sort
+	cout << testLL << endl << endl;
+
+	cout << "insert(3)" << endl;
+	testLL.Insert(3); // add a new int(3) at the end of LL
+	cout << testLL << endl << endl;
+
+	cout << "+= 1" << endl;
+	int scalar = 1;
+	testLL += scalar; // add a new node(1) at the end of LL
+	cout << testLL << endl << endl;
+
+	cout << "[3] = " << testLL[3] << endl << endl;
+
+	{
+		LL testLL2;
+		testLL2.Insert(0, 3, 4); // add 3 new int(4) in a empty LL
+		testLL2[1] = 1;
+		testLL2[2] = 3;
+		cout << "LL2 = " << testLL2 << endl << endl;
+
+		cout << "LL1 += LL2" << endl;
+		testLL += testLL2; // concate 2 list
+		cout << "LL1 = " << testLL << endl;
+		cout << "LL2 = " << testLL2 << endl;
+	} // testLL1 become out of scope (destructed)
+
+	cout << "sort" << endl;
+	testLL.Sort(); // apply bubble sort again
+	cout << testLL << endl << endl;
+	{
+		LL testLL3;
+		testLL3.Insert(testLL.Crop(2, 4));
+		cout << "LL3.insert ( LL1.crop(2,4) )\n";
+		cout << "LL1 = " << testLL << endl;
+		cout << "LL3 = " << testLL3 << endl << endl;
+
+		cout << "LL3.Delete(2, 3)" << endl;
+		testLL3.Delete(2, 3); // (pos, lenght, value)
+		cout << "LL3 = " << testLL3 << endl << endl;
+	}
+	cout << "LL1 = " << testLL << endl;
+}
---
+++ Multi-Types Tree.cpp
@@ -0,0 +1,521 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class ListNode {
+	friend class LinkedList;
+public:
+	int data;
+	float dataf;
+	string datas;
+	int id;
+	ListNode* next;
+	ListNode* next1 = 0;
+	bool gg = false;
+	ListNode() :data(), id(), next() {};
+	ListNode(int a) :data(a), next() {};
+	ListNode(float a) :dataf(a), next() {};
+	ListNode(string a) :datas(a), next() {};
+};
+class LL {
+	friend ostream &operator<<(ostream &, const LL&);
+	friend istream &operator>>(istream&, LL&);
+private:
+	ListNode* first;
+public:
+	LL Crop(int a, int b) {
+		ListNode* current = first, *gg = 0;
+		LL abc;
+		for (int i = 1; i < a; i++) {
+			gg = current->next;
+			current = current->next;
+		}
+		current = current->next;
+		for (int i = 1; i <= b; i++) {
+			abc.Insert(current->data);
+			current = current->next;
+		}
+		gg->next = current;
+		return abc;
+	}
+	void operator+=(int a)
+	{
+		Insert(a);
+	}
+	void Insert(LL a)
+	{
+		ListNode* current = first;
+		if (first == 0) {
+			first = a.first;
+			return;
+		}
+		while (current->next != 0) {
+			current = current->next;
+		}
+		current->next = a.first;
+	}
+	void operator+=(LL a)
+	{
+		ListNode* current = first;
+		while (current->next != 0) {
+			current = current->next;
+		}
+		current->next = a.first;
+	}
+	//testLL2[1] = 1;
+	LL &operator=(int a) {
+		*this = a;
+	}
+	int &operator[](int sub) {
+		ListNode* current = first;
+		for (int i = 1; i <= sub; i++) {
+			current = current->next;
+		}
+		return current->data;
+	}
+	LL() :first() {};
+	LL(int a) :first() {};
+	LL(float a) :first() {};
+	LL(string a) :first() {};
+	void ver() {
+		LL newnode;
+		while (first->next != 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+		}
+		newnode.Insert(first->data);
+		first = first->next;
+		*this = newnode;
+	}
+	void Insert1(int x) {
+		ListNode *newNode = new ListNode(x);
+		newNode->gg = true;
+		if (first == 0) {                      // 若list沒有node, 令newNode為first
+			first = newNode;
+			return;
+		}
+		ListNode *current = first;
+		while (current->next != 0) {           // Traversal
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+	int pop() {
+		LL current;
+		current.first = first;
+		while (current.first->next != 0)
+			current.first = current.first->next;
+		Delete(getLength() - 1, 1);
+		return current.first->data;
+	}
+	void Insert(int x) {//等於void push(int)
+		ListNode *newNode = new ListNode(x);
+		if (first == 0) {                      // 若list沒有node, 令newNode為first
+			first = newNode;
+			return;
+		}
+		ListNode *current = first;
+		while (current->next != 0) {           // Traversal
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+	void Insertf(float x) {//等於void push(int)
+		ListNode *newNode = new ListNode(x);
+		if (first == 0) {                      // 若list沒有node, 令newNode為first
+			first = newNode;
+			return;
+		}
+		ListNode *current = first;
+		while (current->next != 0) {           // Traversal
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+	void Inserts(string x) {//等於void push(int)
+		ListNode *newNode = new ListNode(x);
+		if (first == 0) {                      // 若list沒有node, 令newNode為first
+			first = newNode;
+			return;
+		}
+		ListNode *current = first;
+		while (current->next != 0) {           // Traversal
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+	void Insert(int x, int y) {//x插入的位置 y插入的值
+		LL newnode;
+		while (x >= 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+			x--;
+		}
+		newnode.Insert(y);
+		while (first != 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+		}
+		*this = newnode;
+	}
+	void Insert(int x, int y, int z) {//x插入的位置 y為長度 z插入的值
+		LL newnode;
+		if (first == 0) {
+			for (int i = 1; i <= y; i++)
+				newnode.Insert(z);
+			first = newnode.first;
+			return;
+		}
+		while (x >= 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+			x--;
+		}
+		while (y > 0) {
+			newnode.Insert(z);
+			y--;
+		}
+		while (first != 0) {
+			newnode.Insert(first->data);
+			first = first->next;
+		}
+		*this = newnode;
+	}
+	void Delete(int x, int y) {//x插入的位置 y為長度
+		LL newnode;
+		ListNode *abc = this->first;
+		while (x >= 1) {
+			if (abc->gg == true)
+				newnode.Insert1(first->data);
+			else
+				newnode.Insert(first->data);
+			abc = abc->next;
+			first = first->next;
+			x--;
+		}
+		while (y > 0) {
+			first = first->next;
+			abc = abc->next;
+			if (first == 0)
+				break;
+			y--;
+		}
+		while (first != 0) {
+			if (abc->gg == true)
+				newnode.Insert1(first->data);
+			else
+				newnode.Insert(first->data);
+			abc = abc->next;
+			first = first->next;
+		}
+		*this = newnode;
+	}
+	int &Head() {
+		int a = first->data;
+		headPtr = first;
+		return a;
+	}
+	int &Tail() {
+		ListNode* current = first;
+		while (current->next != 0) {
+			current = current->next;
+		}
+		int a = current->data;
+		tailPtr = current;
+		return a;
+	}
+	int getLength() {
+		ListNode* current = first;
+		int i = 0;
+		while (current->next != 0) {
+			current = current->next;
+			i++;
+		}
+		i++;
+		return i;
+	}
+	void Sort() {
+		ListNode* current = first;
+		for (int j = 1; j < getLength(); j++) {
+			for (int k = 1; k < getLength(); k++) {
+				if (current->data < current->next->data) {
+					swap(k);
+					current = current->next;
+				}
+				else current = current->next;
+			}
+			current = first;
+		}
+	}
+	void swap(int k) {
+		ListNode* current = first;
+		LL abc;
+		for (int i = 1; i < k; i++) {
+			abc.Insert(current->data);
+			current = current->next;
+		}
+		int a = current->data, b = current->next->data;
+		abc.Insert(b);
+		abc.Insert(a);
+		current = current->next;
+		while (current->next != 0) {
+			current = current->next;
+			abc.Insert(current->data);
+		}
+		first = abc.first;
+	}
+	int time() {
+		int ans = 0;
+		ListNode* current = first;
+		if (current->next == 0) {
+			ans = 1;
+			return ans;
+		}
+		while (current != 0) {
+			ans++;
+			if (current->gg == true) {
+				break;
+			}
+			current = current->next;
+		}
+		return ans;
+	}
+	void Sort1() {
+		LL *current = this, abc, line; bool f = false;
+		line.first = first;
+		ListNode *a = first;
+		int i = getLength();
+		for (int j = 1; j < i; j++) {
+			for (int k = 1; k < i; k++) {
+				for (int k = 1; k < i; k++) {
+					if (line.first->next == 0)
+						break;
+					if (a->data < line.first->next->data) {
+						current->Delete(j - 1, 1);
+						if (a->gg == true)
+							current->Insert1(a->data);
+						else
+							current->Insert(a->data);
+						line = *current;
+						break;
+					}
+					else
+						line.first = line.first->next;
+				}
+				if (line.first->next == 0)
+					break;
+				for (int k = 1; k < j; k++) {
+					line.first = line.first->next;
+				}
+				a = line.first;
+			}
+			if (a->gg == true)
+				abc.Insert1(a->data);
+			else
+				abc.Insert(a->data);
+			line = *current;
+			for (int k = 1; k <= j; k++) {
+				line.first = line.first->next;
+			}
+			a = line.first;
+		}
+		if (a->gg == true)
+			abc.Insert1(a->data);
+		else
+			abc.Insert(a->data);
+		*this = abc;
+	}
+	bool isEmpty() {
+		if (first == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool search(int value, int &pos) {
+		LL current; pos = 0;
+		current.first = first;
+		while (current.first != 0) {
+			pos++;
+			if (current.first->data == value) {
+				cout << "找到了!!\n";
+				return true;
+			}
+			current.first = current.first->next;
+		}
+		cout << "沒找到喔\n";
+		return false;
+	}
+	void insertiddata(int a) {
+		first->id = a;
+	}
+	void link(LL a) {
+		this->first->next = a.first;
+	}
+	void link1(LL a) {
+		this->first->next1 = a.first;
+	}
+	void printt() {
+		LL abc = *this;
+		LL abcc = *this;
+		if (first->data == 0) {
+			if (first->dataf == 0) {
+				cout << first->datas << ", ";
+			}
+			else {
+				cout << first->dataf << ", ";
+			}
+		}
+		else {
+			cout << first->data << ", ";
+		}
+		if (abc.first->next != 0) {
+			abc.first = abc.first->next;
+			abc.printt();
+		}
+		if (abcc.first->next1 != 0) {
+			abcc.first = abcc.first->next1;
+			return abcc.printt();
+		}
+	}
+	void printid() {
+		LL abc = *this;
+		LL abcc = *this;
+		if (first->data == 0) {
+			if (first->dataf == 0) {
+				cout << first->id << ", ";
+			}
+			else {
+				cout << first->id << ", ";
+			}
+		}
+		else {
+			cout << first->id << ", ";
+		}
+		if (abc.first->next != 0) {
+			abc.first = abc.first->next;
+			abc.printid();
+		}
+		if (abcc.first->next1 != 0) {
+			abcc.first = abcc.first->next1;
+			return abcc.printid();
+		}
+	}
+	void printr() {
+		LL abc = *this;
+		LL abcc = *this;
+		if (first->data == 0) {
+			if (first->dataf == 0) {
+				for (int i = 0; i < first->datas.size() - 1; i++)
+					cout << first->datas[i];
+				cout<< ", ";
+			}
+			else {
+				cout << first->dataf-0.1 << ", ";
+			}
+		}
+		else {
+			cout << first->data-1 << ", ";
+		}
+		if (abc.first->next != 0) {
+			abc.first = abc.first->next;
+			abc.printr();
+		}
+		if (abcc.first->next1 != 0) {
+			abcc.first = abcc.first->next1;
+			return abcc.printr();
+		}
+	}
+	bool truee() {
+		if (first->next == 0) {
+			return true;
+		}
+		else
+			return false;
+	}
+	bool ee() {
+		if (first != 0) {
+			return true;
+		}
+		else
+			return false;
+	}
+	ListNode* headPtr;
+	ListNode* tailPtr;
+};
+
+ostream& operator<<(ostream& output, const LL& abc) {
+	if (abc.first == 0) {                      // 如果first node指向NULL, 表示list沒有資料
+		output << "List is empty.\n";
+		return output;
+	}
+	ListNode *current = abc.first;            // 用pointer *current在list中移動
+	while (current != 0) {                 // Traversal
+		output << current->data;
+		if (current->next != 0)
+			output << ", ";
+		current = current->next;
+	}
+	output << endl;
+	return output;
+}
+
+int main()
+{
+	int noden, linkn, idd, dataa, fid, cid;
+	string type;
+	ifstream inFile("input_tree_6_2.txt", ios::in);
+	inFile >> noden >> linkn;
+	LL *a = new LL[noden + 1]();
+	for (int i = 0; i < noden; i++) {
+		inFile >> idd >> type;
+			if (type == "int") {
+				int dataa;
+				inFile >> dataa;
+				a[idd].Insert(dataa);
+				a[idd].insertiddata(idd);
+			}
+			else if (type == "float") {
+				float dataa;
+				inFile >> dataa;
+				a[idd].Insertf(dataa);
+				a[idd].insertiddata(idd);
+			}
+			else if (type == "str") {
+				string dataa;
+				inFile >> dataa;
+				a[idd].Inserts(dataa);
+				a[idd].insertiddata(idd);
+			}
+	}
+	for (int i = 0; i < linkn; i++) {
+		inFile >> fid >> cid;
+		if (a[fid].truee()) {
+			a[fid].link(a[cid]);
+		}
+		else {
+			a[fid].link1(a[cid]);
+		}
+	}
+	if (a[0].ee()) {
+		a[0].printid();
+		cout << "\b\b \n";
+		a[0].printt();
+		cout << "\b\b \n";
+		a[0].printr();
+		cout << "\b\b \n";
+	}
+	else {
+		a[1].printid();
+		cout << "\b\b \n";
+		a[1].printt();
+		cout << "\b\b \n";
+		a[1].printr();
+		cout << "\b\b ";
+	}
+	
+}
